1fe0ca8db208085f3927e699e646ab6b77efdc4d
evsmirnovalek <evsmirnovalek@ozon.ru>
2026-03-20 18:30:34 +0300

    changed path

--- Dataset/__init__.py
+++ Dataset/__init__.py
@@ -3,4 +3,4 @@ from pathlib import Path
 # Корневые пути к папкам с аудио файлами
 DATASET_PATH = Path("meld/meld.csv")
 AUDIO_DIR = Path("/Users/evsmirnovalek/PycharmProjects/apollo/Dataset/meld/audio")
-SAMPLES_PATH = Path("models/samples.pkl")
+SAMPLES_PATH = Path("meld/samples.pkl")
